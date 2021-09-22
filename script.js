5bd3035d5059bb5310f8c9d4b132f6f134291641
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-09-22 08:36:32 +0545

    quotes fetched

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// let apiQuotes = [];
+
+function newQuote() {
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  console.log(quote);
+}
+
+// const getQuotes = async () => {
+//   const apiUrl = 'https://type.fit/api/quotes';
+//   const response = await fetch(apiUrl);
+//   apiQuotes = await response.json();
+
+//   newQuote();
+// };
+
+// getQuotes();
+
+newQuote();
