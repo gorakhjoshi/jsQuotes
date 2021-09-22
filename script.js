bb61a70c1b4c71b95a9cdc5bf925e5eb4fdda1c0
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-09-22 08:47:04 +0545

    dynamic quotes

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 'use strict';
 
-// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
 
 function newQuote() {
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  console.log(quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  quoteText.textContent = quote.text;
+  authorText.textContent = quote.author;
 }
 
-// const getQuotes = async () => {
-//   const apiUrl = 'https://type.fit/api/quotes';
-//   const response = await fetch(apiUrl);
-//   apiQuotes = await response.json();
+const getQuotes = async () => {
+  const apiUrl = 'https://type.fit/api/quotes';
+  const response = await fetch(apiUrl);
+  apiQuotes = await response.json();
 
-//   newQuote();
-// };
+  newQuote();
+};
 
-// getQuotes();
-
-newQuote();
+getQuotes();
